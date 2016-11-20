e0e158c141f657c5ba6f3f126742fa4cc43acb33
Nick Zatsepilov <clanat001@gmail.com>
2016-11-21 01:38:20 +0600

    Some parsing stuff for process info, memory regions

---
+++ Sources/MemoryRegion.swift
@@ -0,0 +1,88 @@
+//
+//  MemoryRegion.swift
+//  Smart
+//
+//  Created by Nick Zatsepilov on 20/11/16.
+//
+//
+
+import Foundation
+import Darwin
+
+enum MemoryRegionAccess: String {
+    case read = "r"
+    case write = "w"
+    case execute = "x"
+    case shared = "s"
+    case private_ = "p"
+}
+
+enum MemoryRegionField: Int {
+    case adressSpace
+    case permissions
+    case offset
+    case device
+    case iNode
+    case path
+}
+
+struct MemoryRegion {
+    
+    
+    let memorySpace: Range<UInt>
+    let offset: UInt
+    let deviceMinor: UInt16
+    let deviceMajor: UInt16
+    let iNode: UInt
+    let permissions: [MemoryRegionAccess]
+    
+    private let path: String
+    
+//    static func find(mapFilePath: String, address: UInt) -> MemoryRegion? {
+//        guard FileManager.default.isReadableFile(atPath: mapFilePath) else {
+//            return nil
+//        }
+//        
+//        do {
+//            let lines = try String(contentsOfFile: mapFilePath).components(separatedBy: .newlines)
+//            
+//            for line in lines {
+//                let elements = line.components(separatedBy: .whitespaces)
+//                for i in 0...elements.count {
+//                    guard let field = MemoryRegionField(rawValue: i) else {
+//                        continue
+//                    }
+//                    
+//                    switch field {
+//                    case .:
+//                        <#code#>
+//                    default:
+//                        <#code#>
+//                    }
+//                }
+//                
+//            }
+//            
+//        } catch {
+//            print("Unable to read map file: \(mapFilePath)")
+//        }
+//        
+//        return nil
+//    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Sources/Process.swift
+++ Sources/Process.swift
@@ -10,9 +10,13 @@ import Foundation
 
 typealias ProcessId = UInt
 
-let procDirectory = "/proc/"
+enum ProcessInfoFields: Int {
+    case id
+    case name
+    case state
+}
 
-enum ProcessState: Character {
+enum ProcessState: String {
     case running = "R"
     case sleeping = "S"
     case waiting = "D"
@@ -22,34 +26,84 @@ enum ProcessState: Character {
 }
 
 struct Process {
+    
     let id: ProcessId
-    let directory: String
+    let directoryPath: String
+    let statFilePath: String
+    let mapFilePath: String
+    
+    var isValid: Bool {
+        let fileManager = FileManager.default
+        let statFileReadable = fileManager.isReadableFile(atPath: self.statFilePath)
+        let mapFileReadable = fileManager.isReadableFile(atPath: self.mapFilePath)
+        
+        return statFileReadable && mapFileReadable
+    }
+    
+    var info: ProcessInfo? {
+        return ProcessInfo(process: self)
+    }
     
     init?(id: ProcessId) {
-        guard id.isValid else { return nil }
-        
         self.id = id
-        self.directory = procDirectory + "\(id)/"
+        
+        guard Process.isValid(id: id) else {
+            print("Invalid process id")
+            return nil
+        }
+        
+        directoryPath = "/proc/\(id)/"
+        statFilePath = directoryPath + "stat"
+        mapFilePath = directoryPath + "map"
     }
-}
-
-extension ProcessId {
-    var isValid: Bool {
-        return FileManager.default.fileExists(atPath: procDirectory + "\(self)/stat")
+    
+    static func isValid(id: ProcessId) -> Bool {
+        return FileManager.default.isReadableFile(atPath: "/proc/\(id)/stat")
     }
 }
 
 struct ProcessInfo {
-    let id: ProcessId?
-    let name: String
-    let state: ProcessState
+    let process: Process
+    private(set) var id: ProcessId!
+    private(set) var name: String!
+    private(set) var state: ProcessState!
     
-    init?(id: ProcessId) {
-        guard id.isValid else { return nil }
+    init?(process: Process) {
+        guard process.isValid else {
+            print("Invalid process")
+            return nil
+        }
         
-        self.id = id
-        self.name = ""
-        self.state = .running
+        self.process = process
+        
+        do {
+            let file = try String(contentsOfFile: process.statFilePath)
+            let elements = file.components(separatedBy: " ")
+            
+            
+            
+            for i in 0...elements.count {
+                let value = elements[i]
+                
+                guard let field = ProcessInfoFields(rawValue: i) else {
+                    break
+                }
+                
+                switch field {
+                case .id:
+                    self.id = ProcessId(value)!
+                    break
+                case .name:
+                    self.name = value
+                    break
+                case .state:
+                    self.state = ProcessState(rawValue: value)!
+                    break
+                }
+            }
+        } catch let error {
+            print("Failed reading process stat file: \(error.localizedDescription)")
+        }
     }
 }
 
